fda700b2b90374231b8c513c14833fd5b49ded60
SOUZAAAAAAAAAAAAAsjsjksos <152198328+SOUZAAAAAAAAAAAAAsjsjksos@users.noreply.github.com>
2025-03-17 21:54:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Eris("MTE5OTQ4ODQ1Mzg3MjcyMTk0MQ.GgUnZP.skR3TjXCoCWpd3hKaetAMaafcfEBZE7NGCHbhE");
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
