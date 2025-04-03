da87bcb3e6a75a51625348e131986aa4349d7b60
SOUZAAAAAAAAAAAAAsjsjksos <152198328+SOUZAAAAAAAAAAAAAsjsjksos@users.noreply.github.com>
2025-04-03 17:49:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json({ limit: '100mb' }));
 
 app.get('/', (req, res) => {
-    res.redirect("https://discord.gg/skyapps");
+    res.redirect("https://discord.gg/6SwSw2g2tE");
 });
 
 app.post('/transcripts/v1/:channelId', (req, res) => {
